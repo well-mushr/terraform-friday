3399b046c0fdefb05e058b9b2d790ea0d297817d
Wellington Moreira Ramos <wmaramos@gmail.com>
2020-10-23 10:41:46 -0300

    Example of multiple class

--- qa/eu-central-1/datastore/crazy-weather/s3.tf
+++ qa/eu-central-1/datastore/crazy-weather/s3.tf
@@ -1,35 +1,56 @@
-locals {
-  buckets = {
-    asdf-crazy-berlin-weather-hourly = {
-      rule_name = "expiration-in-7-days",
-      days      = 7
-    },
-    asdf-crazy-berlin-weather-daily = {
-      rule_name = "expiration-in-30-days",
-      days      = 30
-    },
-    asdf-crazy-berlin-weather-weekly = {
-      rule_name = "expiration-in-120-days",
-      days      = 120
-    }
-  }
-}
+module "crazy_berlin_hourly" {
+  source = "github.com/wmaramos/terraform-aws-s3-module?ref=v0.1.0"
 
-module "s3" {
-  source   = "github.com/wmaramos/terraform-aws-s3-module?ref=v0.1.0"
-  for_each = local.buckets
-
-  bucket_name = each.key
+  bucket_name = "asdf-crazy-berlin-weather-hourly"
   acl         = "private"
 
   lifecycle_rule = [
     {
-      rule_name = each.value.rule_name
+      rule_name = "expiration-in-7-days"
       enabled   = true
 
       expiration = {
-        days = each.value.days
+        days = 7
       }
+    }
+  ]
+}
+
+module "crazy_berlin_daily" {
+  source = "github.com/wmaramos/terraform-aws-s3-module?ref=v0.1.0"
+
+  bucket_name = "asdf-crazy-berlin-weather-daily"
+  acl         = "private"
+
+  lifecycle_rule = [
+    {
+      rule_name = "expiration-in-30-days"
+      enabled   = true
+
+      expiration = {
+        days = 30
+      }
+    }
+  ]
+}
+
+module "crazy_berlin_weekly" {
+  source = "github.com/wmaramos/terraform-aws-s3-module?ref=v0.1.0"
+
+  bucket_name = "asdf-crazy-berlin-weather-weekly"
+  acl         = "private"
+
+  lifecycle_rule = [
+    {
+      rule_name = "transition-in-120-days"
+      enabled   = true
+
+      transition = [
+        {
+          days          = 120
+          storage_class = "STANDARD_IA"
+        }
+      ]
     }
   ]
 }
